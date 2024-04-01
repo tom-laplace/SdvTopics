80c2293d6c30185902462e29417358b4fee14186
tomlap <tomlaplace0902@gmail.com>
2024-04-01 15:03:09 +0200

    dockerfile prod ok

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import http from 'http';
 import { handler } from './build/handler.js';
 import express from 'express';
 import { Server } from 'socket.io';
+import { initSubscription, publishMessage } from './pubsub.js';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,17 +11,12 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-	let username = `User ${Math.round(Math.random() * 999999)}`;
-	socket.emit('name', username);
-
 	socket.on('message', (message) => {
-		io.emit('message', {
-			from: username,
-			message: message,
-			time: new Date().toLocaleString()
-		});
+		publishMessage(message.content, message.username);
 	});
 });
+
+initSubscription(io);
 
 console.log('SocketIO injected');
 
